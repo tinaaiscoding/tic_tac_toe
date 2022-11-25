7a9b84552d69c38f31f4cd379d4ff2b4cbd1f22e
tinanvo <tinanhivo@gmail.com>
2022-11-25 21:53:01 +1100

    Updated HTML, CSS, JS files
    Created hamburger menu
    Added function for first two items on the menu: "New Game" and "How to play"
    Created overlay modal for "How to play"
    Added function on "back" buttons, and cancel function if click on backdrop

--- hamburger_menu.js
+++ hamburger_menu.js
@@ -1,12 +1,56 @@
-const menuList = document.querySelector('.ham-menu ul');
-const menuItems = document.querySelectorAll('.ham-menu li')
 const menu = document.querySelector('nav h4');
+const menuList = document.querySelector('.ham-menu ul');
+const menuItems = document.querySelectorAll('.ham-menu li');
+const allModals = document.querySelectorAll('.modal');
+const backdrop = document.getElementById('backdrop');
+const passiveBtn = document.querySelectorAll('.btn-passive')
 
-menu.addEventListener('click', function() {
-  menuList.classList.remove('display-none')
-})
+function backDropOn() {
+  backdrop.classList.add('visible');
+}
 
-menuList.addEventListener('mouseleave', function() {
+function backDropOff() {
+  backdrop.classList.remove('visible');
+}
+
+function removeBackdropHandler() {
+  allModals.forEach(modal => {
+    modal.classList.remove('visible')
+  });
+  backDropOff();
+}
+
+
+  for (let i = 0; i < passiveBtn.length; i++) {
+    passiveBtn[i].addEventListener('click', () => {
+      removeBackdropHandler();
+    });
+  }
+ 
+
+backdrop.addEventListener('click', removeBackdropHandler);
+
+menu.addEventListener('click', function () {
+  menuList.classList.remove('display-none');
+});
+
+menuList.addEventListener('mouseleave', function () {
   menuList.classList.add('display-none');
-})
+});
 
+menuItems[0].addEventListener('click', () => {
+  window.location.reload();
+});
+
+menuItems[1].addEventListener('click', () => {
+  const howToPlayModal = document.querySelector('#how-to-play-modal');
+
+  howToPlayModal.classList.add('visible');
+  backDropOn();
+});
+
+menuItems[2].addEventListener('click', () => {});
+
+menuItems[3].addEventListener('click', () => {});
+
+menuItems[4].addEventListener('click', () => {});
--- modals.js
+++
@@ -1,9 +0,0 @@
-const originalDOM = document.body.innerHTML;
-
-function backDropOn() {
-  backdrop.classList.add('visible');
-}
-
-function backDropOff() {
-  backdrop.classList.remove('visible');
-}
