9c9b3627e56bea5a69638f8cf004b5dad6e81b9f
tinanvo <tinanhivo@gmail.com>
2022-12-04 11:45:18 +1100

    Updated JS Files
    Created "play again" logic

--- modals.js
+++ modals.js
@@ -1,13 +1,30 @@
 const playAgainBtn = document.querySelector('.btn-play-again');
-const pTags = document.querySelectorAll('p');
+// const gameGrid = document.querySelector('.game-grid');
 
 playAgainBtn.addEventListener('click', () => {
+  const pTags = document.querySelectorAll('.game-grid  p');
+  const h3Tags = document.querySelectorAll('.game-grid  h3');
+
+  clickCount = 0;
+
+  pTags.forEach((pTag) => {
+    pTag.remove();
+  });
+
+  h3Tags.forEach((h3tag) => {
+    h3tag.remove();
+  });
 
   questionBlocks.forEach((block) => {
-    block.classList.remove("display-none")
+    block.classList.remove('display-none');
   });
 
-  pTags.forEach((tag) => {
-    tag.remove();
+  gridAllBoxes.forEach((box) => {
+    box.classList.remove('disabled');
   });
-})
+
+  // if (box.firstChild.tagName.toLowerCase() === 'h3') {
+
+  // // console.log(box.firstChild.className )å;
+  // }
+});
